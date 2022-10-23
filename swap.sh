90179c5ed4dfd014a876ee8b1a5657a1f395dbde
Debargha Dutta <debarghadutta2001@gmail.com>
2022-10-23 13:34:47 +0530

    add swap.sh

---
+++ swap.sh
@@ -0,0 +1,13 @@
+#program to swap 2 numbers
+first=10
+second=20
+echo "Before Swapping:"
+echo $first
+echo $second
+
+temp=$first
+first=$second
+second=$temp
+echo "After Swapping:"
+echo $first
+echo $second
---
+++ variables.sh
@@ -0,0 +1,10 @@
+#program to declare, assign operating with variables
+a=10
+b=20
+echo $a
+echo $b
+sum=$(($a + $b))
+echo "The sum is:"
+echo $sum
+name="Debargha"
+echo $name
