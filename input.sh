9f7cc7bf0fe4f8e46c6f882a11161d5cea6f1c78
Debargha Dutta <debarghadutta2001@gmail.com>
2022-10-27 12:22:37 +0530

    add input.sh

---
+++ input.sh
@@ -0,0 +1,7 @@
+#a program to learn how to take input
+#-p flag is used to prompt with input
+read -p "Username: " username
+#-s flag is used to make the input silent
+read -sp "Password: " pass
+echo
+echo "user $username added."
